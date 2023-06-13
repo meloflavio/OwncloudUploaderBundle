77121ff1adabd43de321bade1e2e2532ddc61ae5
meloflavio <meloflavio@gmail.com>
2023-06-13 09:06:20 -0300

    change name class MeloFlavioFileDownloadType

--- Form/Type/MeloFlavioFileDownloadType.php
+++ Form/Type/MeloFlavioFileDownloadType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MeloFlavioInternalFileDownloadType extends AbstractType
+class MeloFlavioFileDownloadType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver)
     {
